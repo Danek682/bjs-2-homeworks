2325b532b5a4115b5f3bdfaf372cd59333a39496
Danek <danil.lapshin228@mail.ru>
2025-03-04 19:54:14 +0600

    Готово первое задание

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -35,33 +35,33 @@ class PrintEditionItem {
  }
  
  class Book extends PrintEditionItem {
-   constructor(name, releaseDate, pagesCount, author) {
-     super(name, releaseDate, pagesCount)
-     this.author = author
-     this.type = "book"
-   }
- }
- 
- class NovelBook extends Book {
-   constructor(name, releaseDate, pagesCount, author) {
-     super(name, releaseDate, pagesCount, author)
-     this.type = "novel"
-   }
- }
- 
- class FantasticBook extends Book {
-   constructor(name, releaseDate, pagesCount, author) {
-     super(name, releaseDate, pagesCount, author)
-     this.type = "fantastic"
-   }
- }
- 
- class DetectiveBook extends Book {
-   constructor(name, releaseDate, pagesCount, author) {
-     super(name, releaseDate, pagesCount, author)
-     this.type = "detective"
-   }
- }
+  constructor(author, name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.author = author;                   
+      this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "detective";
+  }
+}
  
  const picknick = new FantasticBook(
    "Аркадий и Борис Стругацкие",
